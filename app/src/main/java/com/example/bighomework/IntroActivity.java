e60e250c225bb4237a17384ba9131f6acc23709d
having_salt <1732080133@qq.com>
2022-10-31 15:40:46 +0800

    格式修改

--- app/src/main/java/com/example/bighomework/IntroActivity.java
+++ app/src/main/java/com/example/bighomework/IntroActivity.java
@@ -2,22 +2,25 @@ package com.example.bighomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
+
 public class IntroActivity extends AppCompatActivity {
-    ImageView logo;
-    LottieAnimationView anime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        logo = findViewById(R.id.logo);
-        anime = findViewById(R.id.anime);
+
+        ImageView logo = findViewById(R.id.logo);
+        LottieAnimationView anime = findViewById(R.id.anime);
 
         logo.animate().translationY(2000).setDuration(1000).setStartDelay(4000);
         anime.animate().translationY(1600).setDuration(1000).setStartDelay(4000);
+
+        //startActivity(new Intent(this,LoginActivity.class));
     }
 }
