b6971bd3643ec6c7baa1a2c38ba69e637812dd0c
having_salt <1732080133@qq.com>
2022-11-01 15:15:29 +0800

    添加了资料卡内容

--- app/src/main/java/com/example/bighomework/LoginActivity.java
+++ app/src/main/java/com/example/bighomework/LoginActivity.java
@@ -2,7 +2,16 @@ package com.example.bighomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.bighomework.Service.LoginService;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +19,32 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_tab);
+        EditText accountET = (EditText)findViewById(R.id.login_account);
+        EditText passwordET = (EditText)findViewById(R.id.login_password);
+
+        Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    String account = accountET.getText().toString();
+                    String password = passwordET.getText().toString();
+
+                    boolean isTea = new LoginService().isTeacher(account);
+                    if(isTea){
+                        startActivity(new Intent(LoginActivity.this,TeaMainActivity.class));
+                    }else{
+                        startActivity(new Intent(LoginActivity.this,StuMainActivity.class));
+                    }
+                } catch (RuntimeException e) {
+                    accountET.setText("");
+                    passwordET.setText("");
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
+
 }
---
+++ app/src/test/java/com/example/bighomework/LoginServiceUnitTest.java
@@ -0,0 +1,24 @@
+package com.example.bighomework;
+
+import com.example.bighomework.Service.LoginService;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class LoginServiceUnitTest {
+    @Test
+    public void TestIsTeacher(){
+        LoginService ls = new LoginService();
+        try{
+            Assert.assertTrue(ls.isTeacher("tea123"));
+            Assert.assertFalse(ls.isTeacher("stu123"));
+        }catch (Exception e) {
+           Assert.assertTrue(false);
+        }
+    }
+}
