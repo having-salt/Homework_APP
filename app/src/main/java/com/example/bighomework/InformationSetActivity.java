45a95e5bf71f7f737fe50f99f741116deaf18115
ZephyrFan <759140563@qq.com>
2022-11-03 18:42:36 +0800

    完成帮助页面

--- app/src/main/java/com/example/bighomework/InformationSetActivity.java
+++ app/src/main/java/com/example/bighomework/InformationSetActivity.java
@@ -3,12 +3,16 @@ package com.example.bighomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class ImformationSetActivity extends AppCompatActivity {
+public class InformationSetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imformation_set);
+        setContentView(R.layout.activity_information_set);
+
+
     }
 }
