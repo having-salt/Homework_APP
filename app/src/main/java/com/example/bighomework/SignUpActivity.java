8989979269e683dbada2248b9e12ed8ccd4e22db
ZephyrFan <759140563@qq.com>
2022-11-14 15:13:38 +0800

    注册界面跳转

--- app/src/main/java/com/example/bighomework/SignUpActivity.java
+++ app/src/main/java/com/example/bighomework/SignUpActivity.java
@@ -2,7 +2,11 @@ package com.example.bighomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        EditText accountET = (EditText)findViewById(R.id.login_account);
+        EditText passwordET = (EditText)findViewById(R.id.login_password);
+        EditText sure_passwordET = (EditText)findViewById(R.id.sure_password);
+
+        Button sign_up = (Button) findViewById(R.id.sign_up);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    if(passwordET.getText().toString().equals(sure_passwordET.getText().toString())){
+                        String account = accountET.getText().toString();
+                        String password = passwordET.getText().toString();
+                        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                    }
+
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
