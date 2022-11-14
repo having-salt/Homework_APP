2906317458cc086c2dad5b54f96053a455118d27
having_salt <1732080133@qq.com>
2022-11-14 19:59:17 +0800

    添加了记住密码的功能

--- app/src/main/java/com/example/bighomework/LoginActivity.java
+++ app/src/main/java/com/example/bighomework/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.bighomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.bighomework.service.LoginService;
@@ -19,10 +21,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_tab);
+
+        SharedPreferences.Editor account_xml= getSharedPreferences("account_xml",MODE_PRIVATE).edit();
+        SharedPreferences account_read =getSharedPreferences("account_xml",MODE_PRIVATE);
+
+        CheckBox cb = findViewById(R.id.is_remember);
+
         EditText accountET = (EditText)findViewById(R.id.login_account);
         EditText passwordET = (EditText)findViewById(R.id.login_password);
+        accountET.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(account_read.getString(accountET.getText().toString(),null)!=null){
+                    passwordET.setText(account_read.getString(accountET.getText().toString(),""));
+                }
+            }
+        });
 
         Button login = (Button) findViewById(R.id.login);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +49,10 @@ public class LoginActivity extends AppCompatActivity {
                     String password = passwordET.getText().toString();
 
                     boolean isTea = new LoginService().isTeacher(account);
+                    if(cb.isChecked()&&account_read.getString(accountET.getText().toString(),null)==null){
+                        account_xml.putString(account,password);
+                        account_xml.commit();
+                    }
                     if(isTea){
                         Intent intent = new Intent(LoginActivity.this, TeaMainActivity.class);
                         intent.putExtra("account",account);
@@ -63,4 +84,12 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EditText accountET = (EditText)findViewById(R.id.login_account);
+        EditText passwordET = (EditText)findViewById(R.id.login_password);
+        accountET.setText("");
+        passwordET.setText("");
+    }
 }
