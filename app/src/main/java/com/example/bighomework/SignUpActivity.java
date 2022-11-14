31ffa77657a60d1720597df0612377bcbc19b221
ZephyrFan <759140563@qq.com>
2022-11-14 14:30:47 +0800

    注册界面跳转

---
+++ app/src/main/java/com/example/bighomework/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.bighomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
