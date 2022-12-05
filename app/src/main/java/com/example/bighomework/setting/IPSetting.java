42c56fe35cf0f12ec8cceee3fba7db1abf7360c9
having_salt <1732080133@qq.com>
2022-12-05 23:54:12 +0800

    修改系统设置

--- app/src/main/java/com/example/bighomework/setting/IPSetting.java
+++ app/src/main/java/com/example/bighomework/setting/IPSetting.java
@@ -1,9 +1,9 @@
 package com.example.bighomework.setting;
 
 public class IPSetting {
-    public final static String IP = "43.143.200.225";
-    public final static String port = "3306";
-    public final static String sign_port = "1999";
-    public final static String password = "HIT_app_2022";
-    public final static String username = "app";
+    public  static String IP = "43.143.200.225";
+    public  static String port = "3306";
+    public  static String sign_port = "1999";
+    public  static String password = "HIT_app_2022";
+    public  static String username = "app";
 }
