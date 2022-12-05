810d780d5f3fb670b2b258f8acc4fc1eecd8673a
having_salt <1732080133@qq.com>
2022-12-05 23:49:49 +0800

    签到的远程服务器部分完成

--- app/src/main/java/com/example/bighomework/setting/IPSetting.java
+++ app/src/main/java/com/example/bighomework/setting/IPSetting.java
@@ -3,6 +3,7 @@ package com.example.bighomework.setting;
 public class IPSetting {
     public final static String IP = "43.143.200.225";
     public final static String port = "3306";
+    public final static String sign_port = "1999";
     public final static String password = "HIT_app_2022";
     public final static String username = "app";
 }
