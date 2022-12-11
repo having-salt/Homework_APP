9d52b25f8cc25879bea8e199f67fc345b65e8d38
having_salt <1732080133@qq.com>
2022-12-11 21:09:46 +0800

    记住密码

--- app/src/main/java/com/example/bighomework/LoginActivity.java
+++ app/src/main/java/com/example/bighomework/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.bighomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.bighomework.dao.AccountData;
@@ -26,30 +28,45 @@ public class LoginActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
         }//强制在主线程请求http
 
-        EditText accountET = (EditText)findViewById(R.id.login_account);
-        EditText passwordET = (EditText)findViewById(R.id.login_password);
-
+        EditText accountET = (EditText) findViewById(R.id.login_account);
+        EditText passwordET = (EditText) findViewById(R.id.login_password);
+        passwordET.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                SharedPreferences nw = getSharedPreferences("remember_xml", MODE_PRIVATE);
+                String password = nw.getString(accountET.getText().toString(),"");
+                if(!password.isEmpty()&&b){
+                    passwordET.setText(password);
+                }
+            }
+        });
         Button login = (Button) findViewById(R.id.login);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
+                try {
                     String account = accountET.getText().toString();
                     String password = passwordET.getText().toString();
-//                    if(!new AccountData().isCorrect(account,password)){
-//                        accountET.setText("");
-//                        passwordET.setText("");
-//                        return;
-//                    }
+                    if (!new AccountData().isCorrect(account, password)) {
+                        accountET.setText("");
+                        passwordET.setText("");
+                        return;
+                    }
+                    CheckBox is_remember = findViewById(R.id.is_remember);
+                    if(is_remember.isChecked()){
+                        SharedPreferences.Editor remember_xml = getSharedPreferences("remember_xml", MODE_PRIVATE).edit();
+                        remember_xml.putString(account, password);
+                        remember_xml.apply();
+                    }
                     boolean isTea = new LoginService().isTeacher(account);
                     Intent intent;
-                    if(isTea){
+                    if (isTea) {
                         intent = new Intent(LoginActivity.this, TeaMainActivity.class);
-                    }else{
+                    } else {
                         intent = new Intent(LoginActivity.this, StuMainActivity.class);
                     }
-                    intent.putExtra("account",account);
+                    intent.putExtra("account", account);
                     startActivity(intent);
                 } catch (RuntimeException e) {
                     accountET.setText("");
@@ -66,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
+                try {
                     startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
                 } catch (RuntimeException e) {
                     e.printStackTrace();
